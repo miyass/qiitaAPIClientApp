55426831a5d5e59ae589a9c6f784dd24d5fb3f93
miyass <exe.miyakura0629@gmail.com>
2018-09-24 00:56:32 +0900

    initial commit

---
+++ qiitaAPIClientApp/QiitaInfoTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  QiitaInfoTableViewCell.swift
+//  qiitaAPIClientApp
+//
+//  Created by 宮倉宗平 on 2018/09/24.
+//  Copyright © 2018年 Sohei Miyakura. All rights reserved.
+//
+
+import UIKit
+
+class QiitaInfoTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        print("aaaaa")
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- qiitaAPIClientApp/ViewController.swift
+++ qiitaAPIClientApp/ViewController.swift
@@ -7,19 +7,72 @@
 //
 
 import UIKit
+import Dispatch
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    var postTitle: [String] = []
+    var postUserName: [String] = []
+    
+    @IBOutlet weak var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        table.dataSource = self
+        let nib = UINib(nibName: "QiitaInfoTableViewCell", bundle: nil)
+        table.register(nib, forCellReuseIdentifier: "cell")
+        
+        getPosts()
+        print(postTitle)
+        print(postUserName)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postTitle.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath as IndexPath) as! QiitaInfoTableViewCell
+        
+        cell.titleLabel.text! = postTitle[indexPath.row]
+        cell.nameLabel.text! = postUserName[indexPath.row]
+        
+        return cell
     }
 
-
+    func getPosts() {
+        let url = URL(string: "https://qiita.com/api/v2/items?page=1&per_page=20&query=swift")!
+        let semaphore = DispatchSemaphore(value: 0)
+        let task = URLSession.shared.dataTask(with: url) { (data,response,error) in
+            if error != nil {
+                print(error!)
+                return
+            }
+            do {
+                let postsJson = try JSONSerialization.jsonObject(with: data!) as AnyObject
+                for postJson in postsJson as! [AnyObject] {
+                    let user = postJson["user"] as AnyObject
+                    let name = user["name"] as! String
+                    let title = postJson["title"] as! String
+                    self.postUserName.append(name)
+                    self.postTitle.append(title)
+                }
+                semaphore.signal()
+            } catch let error {
+                print(error)
+            }
+        }
+        task.resume()
+        semaphore.wait()
+    }
 }
 
